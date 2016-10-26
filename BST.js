4422778b59a7eacb83a55a4fecc59c9eea01d32e
吴光富 <guangfu_wu@kingdee.com>
2016-10-26 16:33:16 +0800

    二叉树添加删除操作

--- BST.js
+++ BST.js
@@ -23,17 +23,57 @@ BST.prototype.add = function(data) {
       parent = current;
       if (data < current.data) {
         current = current.left;
-        if (current == null) {
+        if (!current) {
           parent.left = node;
           break;
         }
       } else {
         current = current.right;
-        if (current == null) {
+        if (!current) {
           parent.right = node;
           break;
         }
       }
+    }
+  }
+}
+
+BST.prototype.remove = function(node, value) {
+  if (!node) {
+    return null;
+  }
+  if (node.data === value) {
+    if (!node.left && !node.right) {
+      return null;
+    } else if (!node.left) {
+      return node.right;
+    } else if (!node.right) {
+      return node.left;
+    } else {
+      var tempNode = this.min(node.right);
+      node.data = tempNode.data;
+      node.right = this.remove(node.right,tempNode.data);
+      return node;
+    }
+  } else if (value < node.data) {
+    node.left = this.remove(node.left,value);
+    return node;
+  } else {
+    node.right = this.remove(node.right,value);
+    return node;
+  }
+}
+
+BST.prototype.find = function(value) {
+  var parent = this.root;
+
+  while (true) {
+    if (parent.data === value) {
+      return parent;
+    } else if (parent.data > value) {
+      parent = parent.left;
+    } else {
+      parent = parent.right;
     }
   }
 }
@@ -84,12 +124,16 @@ BST.prototype.max = function(node) {
 
 var bst = new BST();
 bst.add(2);
+bst.add(2);
 bst.add(5);
 bst.add(50);
 bst.add(30);
 bst.add(1);
-// bst.inOrder(bst.root);
-// bst.preOrder(bst.root);
+bst.find(50);
+bst.remove(bst.root, 1);
+bst.inOrder(bst.root);
+// bst.inOrder();
+// bst.preOrder();
 // bst.postOrder(bst.root);
-bst.min(bst.root);
-bst.max(bst.root);
+// bst.min(bst.root);
+// bst.max(bst.root);
